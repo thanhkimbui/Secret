7c5c05502dea4df14b5e9f450206568057e503b1
Kim Thanh Bui <thanh.kimm.bui@gmail.com>
2022-12-30 17:30:19 -0800

    Hash Encryption

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 require("dotenv").config();
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-const { CONNREFUSED } = require("dns");
+const md5 = require("md5");
 
 mongoose.set('strictQuery', true);
 
@@ -24,8 +22,6 @@ const userSchema = new mongoose.Schema(
         password: String
     }
 );
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -53,7 +49,7 @@ app.post("/register", function (req, res) {
     const newUser = new User(
        { 
             email: req.body.username,
-            password: req.body.password
+            password: md5(req.body.password)
        }
     );
 
@@ -68,7 +64,7 @@ app.post("/register", function (req, res) {
 
 app.post("/login", function (req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({ email: username }, function (err, foundUser) {
         if (err) {
